f6fefffd5b905799481ed70337caf7688d0d6c32
beatazalewa <beatazalewa@users.noreply.github.com>
2022-05-29 11:01:04 +0000

    Implementation - __init__ function

--- Klasy/pierwszaklasa.py
+++ Klasy/pierwszaklasa.py
@@ -3,11 +3,14 @@
 # pass może być użyte w ciele (a body, które nic nie robi
 # def odejmij(a, b):
 #     pass
-
+# self repreezentuje instancję klasę. Dzięki użyciu self my możemy mieć dostęp do właściwości i metod klasy w Pythonie.
+# Tworzeniee obiektu realizujemy ptrzez tzw. konstruktor - jest to specjalna metoda, która jest wykonanywana, kiedy tworzymy nasz obiekt. W Pythonie taką funkcję pełni metoda __init__.
 class Paletka:
-    pass
+    def __init__(self, kolor):
+        self.kolor_obiektu = kolor
+        print(f"Utworzyliśmy obiekt o kolorze: {self.kolor_obiektu} - ID: {id(self)}") 
 
-# tworzymy obiekt na podstawie klasy, podajeemy nazwę obiektu (paleetka_a) i wywołujemy konstruktor klasy (Paletka())
+# tworzymy obiekt na podstawie klasy, podajeemy nazwę obiektu (paletka_a) i wywołujemy konstruktor klasy (Paletka())
 # f-string
 # val = 'Python course'
 # print(f"Rezultat zwracany przez naszą zmienną to {val}")
@@ -16,6 +19,15 @@ class Paletka:
 # age = 23
 # print(f"Hello, my name is {name} nad I am {age} years old")
 def testklasy():
-    paletka_a = Paletka()
+    paletka_a = Paletka("czerwony")
+    paletka_b = Paletka("niebieski")
+    print("****************************************************")
     print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
     print(dir(paletka_a))
+    print("****************************************************")
+    print(f"Obiekt typu {type(paletka_b)} zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_b))
+    print("****************************************************")
+    print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
+    print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
+
